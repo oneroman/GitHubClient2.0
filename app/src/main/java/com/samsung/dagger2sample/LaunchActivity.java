3c6a1ba11a2b2bf62a972f69828f37d2f6467c22
Roman <romka.shakhmanaev@gmail.com>
2016-05-29 14:49:27 +0900

    modify launch activity
    modify launch activity

--- app/src/main/java/com/samsung/dagger2sample/LaunchActivity.java
+++ app/src/main/java/com/samsung/dagger2sample/LaunchActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.samsung.dagger2sample.base.BaseActivity;
 import com.samsung.dagger2sample.utils.ActivityUtils;
+import com.samsung.dagger2sample.utils.Logger;
 import com.samsung.dagger2sample.views.UserLoginFragment;
 
 public class LaunchActivity extends BaseActivity {
@@ -12,14 +13,14 @@ public class LaunchActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Logger.d(TAG, "onCreate, savedInstanceState [" + savedInstanceState + "]");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //ButterKnife.bind(this);
 
         if(savedInstanceState == null) {
-//            UserLoginFragment fragment = new UserLoginFragment();
-            BlankFragment fragment = new BlankFragment();
+            UserLoginFragment fragment = new UserLoginFragment();
             ActivityUtils.replaceFragment(getSupportFragmentManager(), fragment, R.id.fragment_container, null );
         }
     }
