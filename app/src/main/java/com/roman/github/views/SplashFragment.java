8286bfa029399f1f94210f8e1d9c783848a059b8
Roman <romka.shakhmanaev@gmail.com>
2016-06-18 18:17:39 +0900

    modify splash screen logic

--- app/src/main/java/com/roman/github/views/SplashFragment.java
+++ app/src/main/java/com/roman/github/views/SplashFragment.java
@@ -1,19 +1,24 @@
 package com.roman.github.views;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
 import com.roman.github.R;
 import com.roman.github.base.BaseFragment;
 import com.roman.github.modules.SplashModule;
 import com.roman.github.presenters.Splash;
 import com.roman.github.utils.ActivityUtils;
 import com.roman.github.utils.Logger;
+import com.roman.github.views.splash.DotsView;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import rx.Observable;
@@ -22,11 +27,21 @@ import rx.functions.Action1;
 import rx.functions.Func0;
 import rx.schedulers.Schedulers;
 
+/**
+ * That Fragment is necessary to perform injection of heavy objects while application start up time and shows animation to user
+ * That shall happen only while application first launch time. as soon as app process exists then injection is not applicable and Fragment will be skipped.
+ */
 public class SplashFragment extends BaseFragment implements Splash.View {
 
     private static final String TAG = SplashFragment.class.getSimpleName();
 
     private Unbinder unbinder;
+
+    @BindView(R.id.vDotsView)
+    DotsView dotsView;
+
+    private ObjectAnimator dotsAnimator;
+    private final static int MAX_INJECTION_DURATION = 1000;
 
     @Inject
     Splash.Presenter mPresenter;
@@ -37,7 +52,7 @@ public class SplashFragment extends BaseFragment implements Splash.View {
 
     @Override
     protected void setupDI() {
-        getObservable(this).subscribe(new Action1<Object>() {
+        getObservable(this).mergeWith(getTimerObservable()).skip(1).subscribe(new Action1<Object>() {
             @Override
             public void call(Object obj) {
                 initialized();
@@ -56,6 +71,7 @@ public class SplashFragment extends BaseFragment implements Splash.View {
         Logger.d(TAG, "onCreateView");
         final View view = inflater.inflate(R.layout.fragment_splash, container, false);
         unbinder = ButterKnife.bind(this, view);
+        animate();
 
         return view;
     }
@@ -73,17 +89,50 @@ public class SplashFragment extends BaseFragment implements Splash.View {
                 Logger.d(TAG, "starts first injection");
 
 //                Debug.startMethodTracing("SplashFragment");
+                long currentTime = System.currentTimeMillis();
                 getAppComponent().plus(new SplashModule(fragment)).inject(fragment);
+                Logger.d(TAG, "injection is done for [" + (System.currentTimeMillis() - currentTime) + "] ms");
 //                Debug.stopMethodTracing();
 
                 return Observable.just(new Object());
             }
-        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    private Observable<Object> getTimerObservable() {
+        return Observable.defer(new Func0<Observable<Object>>() {
+            @Override
+            public Observable<Object> call() {
+                try {
+                    Thread.sleep(MAX_INJECTION_DURATION);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return Observable.just(new Object());
+            }
+        })
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread());
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        dotsAnimator.cancel();
+        dotsAnimator = null;
         unbinder.unbind();unbinder = null;
+    }
+
+    private void animate() {
+        if(dotsAnimator == null) {
+            dotsAnimator = ObjectAnimator.ofFloat(dotsView, DotsView.DOTS_PROGRESS, 0, 1f);
+            dotsAnimator.setDuration(MAX_INJECTION_DURATION);
+            dotsAnimator.setStartDelay(50);
+            dotsAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        }
+        dotsAnimator.cancel();
+        dotsAnimator.start();
     }
 }
---
+++ app/src/main/java/com/roman/github/views/splash/Utils.java
@@ -0,0 +1,14 @@
+package com.roman.github.views.splash;
+
+/**
+ * Created by Anna on 18.06.2016.
+ */
+public class Utils {
+    public static double mapValueFromRangeToRange(double value, double fromLow, double fromHigh, double toLow, double toHigh) {
+        return toLow + ((value - fromLow) / (fromHigh - fromLow) * (toHigh - toLow));
+    }
+
+    public static double clamp(double value, double low, double high) {
+        return Math.min(Math.max(value, low), high);
+    }
+}
